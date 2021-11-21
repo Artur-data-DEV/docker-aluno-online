224c65c1850361ada2891cad63d83755f7ba1c9a
Artur Campos <arturcamposba@gmail.com>
2021-11-21 01:30:39 -0300

    Implementation done for feature #11

--- src/models/nota.js
+++ src/models/nota.js
@@ -6,31 +6,34 @@ class Nota {
         this.a3 = a3;
     }
 
-    mediaFinal() {
+    mediaFinal(){
         return Math.max(
             0.4 * this.a1 + 0.6 * this.a2, 
             0.4 * this.a1 + 0.6 * this.a3, 
             0.4 * this.a3 + 0.6 * this.a2
         );
     }
-
+    
 
     mediaCA() {
         let media = this.mediaFinal()
-        if (media <= 5){
-            return 'MI'
-        }
-        else if (media > 5 && media <= 6.9){
-            return 'MM'
-        }
-        else if (media > 6.9 && media <= 8.9){
-            return 'MS'
-        }
-        else if (media > 8.9 && media <= 10){
+        if (media >= 9 && media <= 10){
             return 'SS'
         }
-        else{
-            return 'Sem nota!'
+        else if (media >= 7 && media <= 8.9){
+            return 'MS'
+        }
+        else if (media >= 5 && media <= 6.9){
+            return 'MM'
+        }
+        else if (media >= 3 && media <= 4.9){
+            return 'MI'
+        }
+        else if (media >= 0.1 && media <= 2.9){
+            return 'II'
+        }
+        else if (media == 0 || media == null){
+            return 'SR'
         }
     }
 }
--- test/unit/specs/nota.test.js
+++ test/unit/specs/nota.test.js
@@ -33,3 +33,33 @@ describe('Calculo da média final', () => {
         expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 7);
     });
 });
+
+describe("Calculo de menção", ()=>{    
+
+    test("9,0 – 10  | SS – Superior", () =>{
+    let nota = new Nota(null, 9, 0, 9)  
+     expect(nota.mediaCA()).toBe('SS')
+    })
+    test("7,0 – 8,9 | MS – Médio Superior", () =>{
+    let nota = new Nota(null, 7, 0, 8)
+     expect(nota.mediaCA()).toBe('MS')
+    })
+    test("5,0 – 6,9 | MM – Médio", () =>{
+    let nota = new Nota(null, 5, 0, 6)
+     expect(nota.mediaCA()).toBe('MM')
+    })
+    test("3,0 – 4,9 | MI – Médio Inferior", () =>{
+    let nota = new Nota(null, 3, 0, 3)
+     expect(nota.mediaCA()).toBe('MI')
+    })
+    test("0,1 – 2,9 | II – Inferior", () =>{
+    let nota = new Nota(null, 1, 0, 1)
+     expect(nota.mediaCA()).toBe('II')
+    })
+    test("0,0       | SR – Sem rendimento", () =>{
+     let nota = new Nota(null, 0, 0, 0 )
+     expect(nota.mediaCA()).toBe('SR')
+     nota = new Nota(null, null, null, null)
+     expect(nota.mediaCA()).toBe('SR')
+    })
+})
